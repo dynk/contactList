49d9df2cda5de1b4e231592c08200de8741da231
Raphael Dynk Nogueira Salvador <Raphael Dynk Nogueira Salvador>
2017-02-12 20:52:50 -0200

    implementing simple list

--- App/MainActivity.cs
+++ App/MainActivity.cs
@@ -1,18 +1,31 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
 
 namespace App
 {
     [Activity(Label = "App", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private List<string> mItems;
+        private ListView mListView;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+
+            mItems = new List<string>();
+            mItems.Add("Dynk");
+            mItems.Add("Fabio");
+            mItems.Add("Lucas");
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+            mListView.Adapter = adapter;
         }
     }
 }
