0004a5778675fad1a3dd50c0d0f9904bded3baf1
Lucas Isabel <lucky.o.isa@gmail.com>
2017-02-13 12:46:38 -0200

    changed method LoadData to return a list of person

--- App/MainActivity.cs
+++ App/MainActivity.cs
@@ -11,6 +11,7 @@ using App.Resources;
 namespace App
 {
     [Activity(Label = "App", MainLauncher = true, Icon = "@drawable/icon")]
+   
     public class MainActivity : Activity
     {
         private List<string> mItems;
@@ -19,11 +20,6 @@ namespace App
         ListView lstData;
         List<Person> lstSource = new List<Person>();
         Database db;
-
-
-
-
-
 
 
         protected override void OnCreate(Bundle bundle)
@@ -36,21 +32,20 @@ namespace App
             db = new Database();
             db.createDataBase();
 
-
+            Person person = new Person();
+                person.name = "Lucas Oliveira";
+                person.email = "lucas.o.isa@hotmail.com";
+                person.celphone = 10101010;
+            db.InsertIntoTablePerson(person);
 
             mListView = FindViewById<ListView>(Resource.Id.myListView);
 
             b = FindViewById<Button>(Resource.Id.button1);
             b.Click += delegate { callPage(); };
-
-            mItems = new List<string>();
-            mItems.Add("Dynk");
-            mItems.Add("Fabio");
-            mItems.Add("Lucas");
-            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+            
+            ArrayAdapter<Person> adapter = new ArrayAdapter<Person>(this, Android.Resource.Layout.SimpleListItem1, LoadData());
             mListView.Adapter = adapter;
-
-
+         
 
             //lstData.ItemClick += (s, e) => {
             //    for (int i = 0; i < lstData.Count; i++)
@@ -60,20 +55,19 @@ namespace App
             //            // vai pra pagina de edicao
             //        }
             //    }
-                
-            //};
 
-            //LoadData();
+        //};
 
-            
+        //LoadData();
+
+
+    }
+
+        private List<Person> LoadData(){
+                lstSource = db.selectTablePerson();
+                return lstSource;
         }
 
-        //private void LoadData()
-        //{
-        //    lstSource = db.selectTablePerson();
-        //    var adapter = new ListViewAdapter(this, lstSource);
-        //    lstData.Adapter = adapter;
-        //}
 
         public void callPage() {
             var intent = new Intent(this, typeof(ContactActivity));
